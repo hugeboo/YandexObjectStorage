13ea52051ab6688a31ba10a667fa59f6721a47a6
hugeboo <sesv@yandex.ru>
2023-04-18 17:17:15 +0300

    Update AboutBox.cs

--- src/Dotkit.YandexObjectStorage.Browser/AboutBox.cs
+++ src/Dotkit.YandexObjectStorage.Browser/AboutBox.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
